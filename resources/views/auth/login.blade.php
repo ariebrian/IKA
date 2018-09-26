f0f807c5dc7df1cb628473050d9c671a2cc3e053
Fatan <fatankaizen@gmail.com>
2018-09-27 00:39:49 +0700

    Just try

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Masuk ke Akun IKA HImatif') }}</div>
+                <div class="card-header">{{ __('Masuk ke Akun IKA Himatif') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
