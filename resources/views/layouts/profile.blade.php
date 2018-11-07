7e694b573f39d28c8b3dc73da675a4dd5d8383ec
Arie Brian <ariebrian22@gmail.com>
2018-11-08 00:49:05 +0700

    lengkapin profil

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -13,12 +13,30 @@
             <p>{{$mahasiswa->nama}}</p>
             <h4>Tanggal lahir:</h4>
             <p>{{Carbon\Carbon::parse($mahasiswa->mhs_tgl_lahir)->format('d F Y')}}</p>
+            <h4>Alamat:</h4>
+            <p>{{$mahasiswa->mhs_alamat_rmh}}</p>
+            <h4>Angkatan:</h4>
+            <p>{{$mahasiswa->mhs_angkatan}}</p>
+            <h4>Bidang Minat:</h4>
+            @if ($mahasiswa->mhs_peminatan == 'SI')
+            <p>Sistem Informasi dan Rekayasa Perangkat Lunak</p>
+            @elseif ($mahasiswa->mhs_peminatan == 'AI')
+            <p>Sistem Cerdas dan Sistem Grafika</p>
+            @else
+            <p>Jaringan Komputer dan Komunikasi Data</p>
+            @endif      
             <h4>Tahun Lulus:</h4>
+            @if($mahasiswa->mhs_thn_lulus == null)
+                <p>Belum Lulus</p>
+            @else    
             <p>{{$mahasiswa->mhs_thn_lulus}}</p>
+            @endif
             <h4>Judul skripsi:</h4>
             <p>{{$mahasiswa->mhs_skripsi}}</p>
             <h4>Kontak:</h4>
             <p>{{$mahasiswa->mhs_no_wa}}</p>  
+            <h4>LinkedIn:</h4>
+            <p>{{$mahasiswa->mhs_linkedin}}</p>  
             @foreach ($pekerjaan as $work)
                 <h4>Tempat kerja:</h4>
                 <p>{{$work['tempat_kerja']}}</p>
