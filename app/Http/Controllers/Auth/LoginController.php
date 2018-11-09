21bebc9b038e546aa95c87aa3d5147e12406acb8
Fatan <fatankaizen@gmail.com>
2018-11-09 15:28:20 +0700

    Change redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/add';
+    protected $redirectTo = '/edit';
 
     /**
      * Create a new controller instance.
