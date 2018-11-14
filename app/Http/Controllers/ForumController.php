dfb6e30537ae97c071a6958e31a61ea11be9329a
Arie Brian <ariebrian22@gmail.com>
2018-11-14 20:47:39 +0700

    masukin forum sama nampilin subforum

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Forum;
 use App\Reply;
+use App\User;
+use App\Mahasiswa;
 
 class ForumController extends Controller
 {
@@ -39,20 +41,43 @@ class ForumController extends Controller
     public function show($id)
     {
         $forum = Forum::find($id);
-        // $replies = Reply::where('forum_id', [$forum->id])->get();
-        // $count = Reply::where('forum_id', [$forum->id])->count();
-        $replies = DB::select('select * from replies where forum_id = ?', [$forum->id]);
+        $creator = User::where('id',$forum->user_id)->first();
+        // $replies = DB::select('select * from replies where forum_id = ?', [$forum->id]);
+        $replies = Reply::where('forum_id',$forum->id)->get();
+        // dd($replies);
         $count = DB::table('replies')->where('forum_id',[$forum->id])->count();
         $data = [
             'forum' => $forum,
+            'creator' => $creator,
             'replies' => $replies,
             'counts' => $count,
         ];
-        dd($data);
+        // dd($data);
+        return view('layouts.subforum', $data);
+        
     }
 
     public function add()
     {
         return view('layouts.addforum');
+    }
+
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        $forum = new Forum;
+
+        $forum->title = $request->judul;
+        $forum->description = $request->deskripsi;
+        $forum->user_id = $user->id;
+        $forum->first_post = " ";
+
+        $forum->save();
+
+        dd($forum);
+        
+        return redirect()->action(
+            'ForumController@show', ['id' => $forum->id]
+        );
     }
 }
