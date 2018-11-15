d91f1802a8c788b8802ddbc6bf831baa8ddd067f
Arie Brian <ariebrian22@gmail.com>
2018-11-15 17:56:17 +0700

    profil fixed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -75,7 +75,7 @@ class ProfileController extends Controller
         // dd($mhs);
         $result =[
             'mahasiswa' => $mahasiswa,
-            'pekerjaan' => $mhs,
+            // 'pekerjaan' => $mhs,
         ];
 
         // dd($result);
